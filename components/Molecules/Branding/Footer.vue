ab4df1ea6f691e9a2f74f0d1043b03a8f9db754b
Andre Abelmann <3215327+aabelmann@users.noreply.github.com>
2022-12-05 14:17:55 +0100

    First version of the footer

---
+++ components/Molecules/Branding/Footer.vue
@@ -0,0 +1,40 @@
+<template>
+  <div v-if="intro" class="bg-black text-white grid grid-cols-12 gap-x-6 gap-y-6 justify-items-center items-center py-4">
+    <div class="col-start-0 col-span-2">
+      <AtomsIcon name="ndeLogo" />
+    </div>
+    <span class="col-start-6 col-span-2">HeritageFlix</span>
+    <span class="col-start-11 col-span-2">[SOCIALIZE ICONS]</span>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { useBrandingStore } from "@/stores/branding";
+const { intro } = useBrandingStore();
+const description = computed(() => intro.description.substring(0, 300));
+</script>
+
+<style lang="scss" scoped>
+// Typography
+.title {
+  font-weight: 600;
+  font-size: 2.75rem;
+  line-height: 125%;
+  text-transform: uppercase;
+}
+
+.description {
+  font-weight: 400;
+  font-size: 1.5rem;
+  line-height: 125%;
+  text-transform: uppercase;
+}
+
+.outro {
+  font-weight: 700;
+  font-size: 1.25rem;
+  line-height: 125%;
+  text-transform: uppercase;
+}
+
+</style>
